cb44b9f0c1945c0efbcd015a068fd52f773242e8
HirunaD <de_silva_pkhd_e22@engug.ruh.ac.lk>
2025-01-03 22:55:37 +0530

    Space

--- src/components/Home/Feedbacks.tsx
+++ src/components/Home/Feedbacks.tsx
@@ -3,7 +3,7 @@ import { reviews } from "../../constants/Home/riviews";
 const Feedbacks = () => {
 
   return (
-    <section className="bg-gray-100 px-6 py-12">
+    <section className="bg-gray-100 px-6 py-12 ">
       <h2 className="text-3xl font-bold text-center mb-8">What Our Customers Say </h2>
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         {reviews.map((review) => (
